08a8a97fc261f943470da401721cd582b6839901
vinicciusnev <xxnashflows@hotmail.com>
2024-03-07 21:25:21 -0300

    feat: Classe onde instancia o AgenteAspirador e executa

---
+++ src/main/java/com/inteligenciaartificial/Main.java
@@ -0,0 +1,10 @@
+package com.inteligenciaartificial;
+
+import com.inteligenciaartificial.strategys.agenteaspirador.AgenteAspirador;
+
+public class Main {
+    public static void main(String[] args) {
+        AgenteAspirador agenteAspirador = new AgenteAspirador();
+        agenteAspirador.executar();
+    }
+}
